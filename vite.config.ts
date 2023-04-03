7efe9431ae90c4955cafa5665073d266656b32c8
Sean Brown <seanbrown82@hotmail.com>
2023-04-03 20:21:40 +0100

    Build: install bootstrap, react bootstrap and testing packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,4 +8,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/space-image-app/",
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  }
 })
