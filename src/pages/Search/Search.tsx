8e132e63a0cd27a2994ef255677690c149e757b8
Sean Brown <seanbrown82@hotmail.com>
2023-04-05 14:50:21 +0100

    Feat: add buttons to search page

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import { Container, Form, Button } from "react-bootstrap"
+import { Container, Button, ButtonGroup } from "react-bootstrap"
 
 export const Search = () => {
   return (
     <Container>
       <h2 className="text-light">Search</h2>
+      <ButtonGroup id="search-button-container" className="align-self-center">
+        <Button className="btn btn-primary" role="button">Random Image</Button>
+        <Button className="btn btn-primary ms-1" role="button">Today's Image</Button>
+
+      </ButtonGroup>
       
     </Container>
   )
--- src/pages/Search/__tests__/Search.test.tsx
+++ src/pages/Search/__tests__/Search.test.tsx
@@ -6,8 +6,20 @@ import { BrowserRouter as Router } from "react-router-dom"
 describe("Search Page", () => {
     it("should display title", () => {
         render(<Search />)
-        const title = screen.getByText("Search")
+        const title = screen.getByText(/Search/i)
         expect(title).toBeDefined()
+    })
+
+    it("should display random button", () => {
+        render(<Search />)
+        const randomButton = screen.getByText(/Random Image/i)
+        expect(randomButton).toBeDefined()
+    })
+
+    it("should display today button", () => {
+        render(<Search />)
+        const todayButton = screen.getByText(/Today's Image/i)
+        expect(todayButton).toBeDefined()
     })
 
 })
