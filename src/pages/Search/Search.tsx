565d371a360cb743970f629b70227e324a0b991f
Sean Brown <seanbrown82@hotmail.com>
2023-04-05 15:19:03 +0100

    Feat: create function to retrieve today's image from the APOD API

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -10,15 +10,30 @@ export const Search = () => {
     try {
 
       const data = await fetch(`https://api.nasa.gov/planetary/apod?count=1&api_key=${import.meta.env.VITE_API_KEY}`)
-      const useData = await data.json()
-      console.log(useData)
-      setImages(useData)
+
+      const imageData = await data.json()
+
+      setImages(imageData)
+
     } catch(e) {
       alert(e)
     }
 
-    
+  }
 
+  async function getTodaysImage() {
+
+    const date = new Date().toJSON().slice(0,10)
+
+    try {
+      const data = await fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${import.meta.env.VITE_API_KEY}`)
+      const imageData = await data.json()
+
+      setImages(imageData)
+    } catch(e) {
+      alert(e)
+    }
+    
   }
 
   return (
@@ -26,7 +41,7 @@ export const Search = () => {
       <h2 className="text-light">Search</h2>
       <ButtonGroup id="search-button-container" className="align-self-center">
         <Button className="btn btn-primary" role="button" onClick={getRandomImage}>Random Image</Button>
-        <Button className="btn btn-primary ms-1" role="button">Today's Image</Button>
+        <Button className="btn btn-primary ms-1" role="button" onClick={getTodaysImage}>Today's Image</Button>
       </ButtonGroup>
       
     </Container>
