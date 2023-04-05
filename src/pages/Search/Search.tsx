b6e44696cc14738841f64c63bc2b00aa8d88d1d2
Sean Brown <seanbrown82@hotmail.com>
2023-04-05 15:06:55 +0100

    Feat: create function to get random image from APOD API

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,13 +1,32 @@
 import { Container, Button, ButtonGroup } from "react-bootstrap"
+import { useState } from "react"
 
 export const Search = () => {
+
+  const [images, setImages] = useState([])
+
+  async function getRandomImage() {
+
+    try {
+
+      const data = await fetch(`https://api.nasa.gov/planetary/apod?count=1&api_key=${import.meta.env.VITE_API_KEY}`)
+      const useData = await data.json()
+      console.log(useData)
+      setImages(useData)
+    } catch(e) {
+      alert(e)
+    }
+
+    
+
+  }
+
   return (
     <Container>
       <h2 className="text-light">Search</h2>
       <ButtonGroup id="search-button-container" className="align-self-center">
-        <Button className="btn btn-primary" role="button">Random Image</Button>
+        <Button className="btn btn-primary" role="button" onClick={getRandomImage}>Random Image</Button>
         <Button className="btn btn-primary ms-1" role="button">Today's Image</Button>
-
       </ButtonGroup>
       
     </Container>
