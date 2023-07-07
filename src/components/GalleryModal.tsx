2e37e4026d22fc4e189284e0c403ed7a5d5e5e77
Sean Brown <seanbrown82@hotmail.com>
2023-07-07 21:58:43 +0100

    Feat: add modal for new gallery creation

--- src/components/GalleryModal.tsx
+++ src/components/GalleryModal.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
-import { Button, Modal } from "react-bootstrap"
+import { Button, Modal, Form } from "react-bootstrap"
 
-export const GalleryModal = () => {
+export const GalleryModal = ({creatingGallery, closeModal, submitFunction} : {creatingGallery: boolean, closeModal: any, submitFunction: any}) => {
+
+    function handleSubmit(e: any) {
+        e.preventDefault();
+        submitFunction();
+    }
   return (
-    <div>GalleryModal</div>
+    <Modal show={creatingGallery} onHide={closeModal}>
+        <Modal.Header closeButton>
+            <Modal.Title>Create New Gallery</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Label>Gallery Name</Form.Label>
+                    <Form.Control type="text" maxLength={20} placeholder="Solar System" />
+                </Form.Group>
+                <Button type="submit" variant="primary">Create</Button>
+            </Form>
+
+        </Modal.Body>
+        
+    </Modal>
   )
 }
--- src/pages/Galleries/Galleries.tsx
+++ src/pages/Galleries/Galleries.tsx
@@ -3,9 +3,11 @@ import { db, setDoc, doc, getDoc, collection, getDocs, addDoc } from "../../conf
 import { AuthContext } from '../../contexts/AuthContext'
 import { Button, Row, Col } from "react-bootstrap"
 import { v4 as uuidv4 } from 'uuid';
+import { GalleryModal } from '../../components/GalleryModal';
 
 export const Galleries = () => {
   const [userGalleries, setUserGalleries] = useState<any>([])
+  const [creatingGallery, setCreatingGallery] = useState<boolean>(false)
   const user = useContext(AuthContext)
 
   async function getGalleries() {
@@ -13,13 +15,20 @@ export const Galleries = () => {
       const galleriesFromDatabase = await getDocs(collection(db, "users", user!.uid, "galleries"))
       const newUserGalleries: Array<any> = [];
       galleriesFromDatabase.forEach(gallery => newUserGalleries.push(gallery.data()));
-      console.log(newUserGalleries)
       setUserGalleries([...newUserGalleries])
     }
     catch(e) {
       console.error(e)
     }
 
+  }
+
+  function openGalleryModal() {
+    setCreatingGallery(true)
+  }
+
+  function closeGalleryModal() {
+    setCreatingGallery(false)
   }
 
   async function createGallery() {
@@ -43,11 +52,10 @@ export const Galleries = () => {
           <h2>Galleries</h2>
         </Col>
         <Col>
-          <Button variant="light" className="w-50" onClick={createGallery}>Create New Gallery</Button>
+          <Button variant="light" className="w-50" onClick={openGalleryModal}>Create New Gallery</Button>
         </Col>
-        
       </Row>
-      
+      {creatingGallery ? <GalleryModal creatingGallery={creatingGallery} closeModal={closeGalleryModal} submitFunction={createGallery}/> : null }
       {userGalleries && userGalleries.length === 0 ? "No galleries found" : "Galleries Found"}
     </div>
   )
