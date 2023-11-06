bf58c3e01443220086f9044ac686cd3a396a17fa
Sean Brown <seanbrown82@hotmail.com>
2023-11-06 19:23:24 +0000

    refactor: update gallerymodal types

--- src/components/ExistingGalleryFields.tsx
+++ src/components/ExistingGalleryFields.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Form } from "react-bootstrap"
+import { IGallery } from '../types/types'
 
-export const ExistingGalleryFields = ({existingGalleryRef, galleries} : {existingGalleryRef: any, galleries: any}) => {
+export const ExistingGalleryFields = ({existingGalleryRef, galleries} : {existingGalleryRef: any, galleries: IGallery[] | null}) => {
   return (
     <Form.Group>
         <Form.Label>Add to Existing Gallery</Form.Label>
         <Form.Select ref={existingGalleryRef}>
             <option value={"none-selected"}>Select a Gallery</option>
-            {galleries && galleries.length > 0 ? galleries.map((gallery: any) => {
+            {galleries && galleries.length > 0 ? galleries.map((gallery: IGallery) => {
                 if (typeof gallery === "object" && !gallery.isDeleted) {
                     return <option value={gallery.id} key={gallery.id}> {gallery.name} </option>}
                     }) : null }
