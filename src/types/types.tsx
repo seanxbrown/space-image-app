6206e1e20929dc62f605d1061333c5bdcb8325fe
Sean Brown <seanbrown82@hotmail.com>
2023-11-06 18:44:49 +0000

    refactor: remove excess spaces and quotation marks

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,15 +1,13 @@
 export interface IPhoto {
-
-    "date": string;
-    "explanation": string;
-    "hdurl": string;
-    "media_type": string;
-    "service_version": string;
-    "title": string;
-    "url": string;
-    "id": string;
-    "isDeleted": boolean
-      
+    date: string;
+    explanation: string;
+    hdurl: string;
+    media_type: string;
+    service_version: string;
+    title: string;
+    url: string;
+    id: string;
+    isDeleted: boolean
 }
 
 export interface IGallery {
