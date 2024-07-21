87ac98f771da585d4d7bd949677cf98763f53c91
Sean Brown <seanbrown82@hotmail.com>
2024-07-21 17:13:44 +0100

    chore: prepare tsconfig file for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/space-image-app/",
   plugins: [react()],
   test: {
     globals: true,
